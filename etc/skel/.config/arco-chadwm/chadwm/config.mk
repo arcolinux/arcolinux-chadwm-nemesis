327240d517cadf64effd62b33cf077012af08ea1
Erik Dubois <erik.dubois@gmail.com>
2024-07-03 07:41:23 +0200

    comments

--- etc/skel/.config/arco-chadwm/chadwm/config.mk
+++ etc/skel/.config/arco-chadwm/chadwm/config.mk
@@ -7,7 +7,10 @@ VERSION = 6.5
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
+# X11 include directory
 X11INC = /usr/include/X11
+
+# X11 library directory
 X11LIB = /usr/lib/X11
 
 # Xinerama, comment if you don't want it
@@ -24,8 +27,14 @@ LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lXrender -lImlib2 -lXe
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
+
+# CFLAGS for debugging
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
+
+# CFLAGS for optimized release
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -O3 -march=native ${INCS} ${CPPFLAGS} -Wno-unused-function
+
+# Linker flags
 LDFLAGS  = ${LIBS}
 
 # compiler and linker
--- etc/skel/.config/arco-chadwm/chadwm/util.h
+++ etc/skel/.config/arco-chadwm/chadwm/util.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
-#define MAX(A, B)               ((A) > (B) ? (A) : (B))
-#define MIN(A, B)               ((A) < (B) ? (A) : (B))
-#define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
+#define MAX(A, B)               ((A) > (B) ? (A) : (B))         // Macro to get the maximum of two values
+#define MIN(A, B)               ((A) < (B) ? (A) : (B))         // Macro to get the minimum of two values
+#define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))      // Macro to check if a value is between two other values
 
-void die(const char *fmt, ...);
-void *ecalloc(size_t nmemb, size_t size);
+void die(const char *fmt, ...);                                 // Function to print an error message and exit
+void *ecalloc(size_t nmemb, size_t size);                       // Function to allocate memory and check for allocation failure
